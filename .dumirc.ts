aec48891972ebf6d7298d8e3790005f8b0e2bda5
dezhizhang <1541609448@qq.com>
2024-10-10 21:13:33 +0800

    feat:interface 和 type 到底有什么区别？

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,35 @@
+/*
+ * :file description:
+ * :name: /fe-interview/.dumirc.ts
+ * :author:张德志
+ * :copyright: (c) 2024, Xiaozhi
+ * :date created: 2024-10-10 20:20:45
+ * :last editor: 张德志
+ * :date last edited: 2024-10-10 21:05:26
+ */
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
   themeConfig: {
-    name: '',
+    mode: 'site',
+    name: '前端面试',
+    antd: {},
+    base: '/',
+    logo: false,
+    history: { type: 'hash' },
+    nav: [
+      {
+        title: 'ECMAScript',
+        link: '/ecmascript',
+      },
+      {
+        title: 'Typescript',
+        link: '/typescript',
+      },
+      {
+        title: 'Webgl',
+        link: '/webgl',
+      },
+    ],
   },
 });
