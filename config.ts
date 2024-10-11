a7871a434faa29dbfa07ea1d42c0c4be357df04e
dezhizhang <1541609448@qq.com>
2024-10-11 11:39:43 +0800

    feat:更新

--- config.ts
+++ config.ts
@@ -5,13 +5,7 @@
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-10-11 11:31:45
  * :last editor: 张德志
- * :date last edited: 2024-10-11 11:31:46
+ * :date last edited: 2024-10-11 11:39:26
  */
-export default {
-    ak: 'LTAI5tSBMXfbmurHE1mhikKy',
-    sk: 'TRM0tkhV9cFNEWuvWVHFtNHjqcNC6T',
-    bucket: 'tugua',
-    region: 'oss-cn-hangzhou', // bucket所在区域的接入点
-  };
-  
+
   
